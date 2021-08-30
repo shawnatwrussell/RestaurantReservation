f3f13c15076bcf61ce9247ad3d6b93539fcb9713
Shawna Russell <shawnatwrussell@outlook.com>
2021-08-30 15:08:33 -0400

    Add project files.

---
+++ RestaurantReservation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestaurantReservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
