193cbe83adc79161cb47b41b31e4637675d3a876
Shawna Russell <shawnatwrussell@outlook.com>
2021-11-02 11:49:35 -0400

    Change footer colors

---
+++ RestaurantReservation/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Services.Interfaces
+{
+    public interface IReservationService
+    {
+
+    }
+}
