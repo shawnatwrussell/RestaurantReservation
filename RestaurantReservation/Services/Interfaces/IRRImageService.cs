715e8b0c0f2de518e8ce2f8ebf8ec26fd4a65f7f
Shawna Russell <shawnatwrussell@outlook.com>
2021-11-04 14:13:29 -0400

    added ImageService & it's interface

---
+++ RestaurantReservation/Services/Interfaces/IRRImageService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Services.Interfaces
+{
+    interface IRRImageService
+    {
+        Task<byte[]> EncodeFileAsync(string fileName);
+        string ContentType(IFormFile file);
+        int Size(IFormFile file);
+
+        bool ValidateFileType(IFormFile file);
+        bool ValidateFileType(IFormFile file, List<string> filetypes);
+
+        bool ValidateFileSize(IFormFile file);
+        bool ValidateFileSize(IFormFile file, int maxSize);
+
+
+        Task<byte[]> EncodeFileAsync(IFormFile File);
+
+        //returning the image/displaying it on the view
+        string DecodeImage(byte[] data, string type);
+
+    }
+}
---
+++ RestaurantReservation/Services/RRImageService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestaurantReservation.Services.Interfaces;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Services
+{
+    public class RRImageService : IRRImageService
+    {
+        private const int DefaultMaxFileSize = 1024;
+
+        public string ContentType(IFormFile file)
+        {
+
+            return file?.ContentType.Split('/')[1];
+        }
+
+        public string DecodeImage(byte[] data, string type)
+
+        {
+            if (data is null || type is null) return null;
+            return $"data:{type};base64,{Convert.ToBase64String(data)}";
+        }
+
+        public async Task<byte[]> EncodeFileAsync(IFormFile File)
+        {
+            if (File is null) return null;
+
+            using var ms = new MemoryStream();
+            await File.CopyToAsync(ms);
+            return ms.ToArray();
+        }
+
+        public async Task<byte[]> EncodeFileAsync(string fileName)
+        {
+            var file = $"{Directory.GetCurrentDirectory()}/wwwroot/dist/img/{fileName}";
+            return await File.ReadAllBytesAsync(file);
+        }
+
+        public async Task<byte[]> EncodeAndReduceFileAsync(IFormFile file)
+        {
+            if (file is null) return null;
+
+            var image = Image.Load(file.OpenReadStream(), out IImageFormat format);
+            image.Mutate(X => X.Resize(new ResizeOptions
+            {
+                Mode = ResizeMode.Min,
+                Size = new Size(DefaultMaxFileSize)
+            }));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var imageEncoder = image.GetConfiguration().ImageFormatsManager.FindEncoder(format);
+                await image.SaveAsync(memoryStream, imageEncoder);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public int Size(IFormFile file)
+        {
+            return Convert.ToInt32(file?.Length);
+        }
+
+        public bool ValidateFileSize(IFormFile file)
+        {
+            return Size(file) < DefaultMaxFileSize;
+        }
+
+        public bool ValidateFileSize(IFormFile file, int maxSize)
+        {
+            return Size(file) < maxSize;
+        }
+
+        public bool ValidateFileType(IFormFile file)
+        {
+            var authorizedTypes = new List<string> { "jpg", "jpeg", "png", "gif" };
+            var validExt = authorizedTypes.Contains(ContentType(file));
+            return validExt;
+        }
+
+        public bool ValidateFileType(IFormFile file, List<string> filetypes)
+        {
+            var validExt = filetypes.Contains(ContentType(file));
+            return validExt;
+        }
+    }
+}
